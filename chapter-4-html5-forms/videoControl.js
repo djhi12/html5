96538b6dad324d3cee27f37cc7b657dc213f94ba
Daniel John Idorot <ido20002@byui.edu>
2022-04-14 12:48:33 +0800

    initial commit

---
+++ chapter-4-html5-forms/videoControl.js
@@ -0,0 +1,14 @@
+const videoEl = document.getElementsByTagName('video')[0], 
+playPauseBtn = document.getElementById('playPause'),
+vidControls = document.getElementById('controls'),
+muteBtn = document.getElementById('muteUnmute'),
+timeHolder = document.getElementById('timer');
+
+videoEl.removeAttribute('controls');
+
+videoEl.addEventListener('canplaythrough', function() {
+    vidControls.classList.remove('hidden');
+}, false);
+
+console.log(videoEl);
+console.log('It is working');
