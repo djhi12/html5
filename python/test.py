05a8ea4047879c5303230243d9b0950e7ec496a1
Daniel John Idorot <ido20002@byui.edu>
2022-04-23 00:05:15 +0800

    initial commit

---
+++ python/test.py
@@ -0,0 +1,10 @@
+print("What is your full name?")
+fullName = input()
+
+print("How old are you?")
+yourAge = input()
+
+print("What is your favorite color?")
+favoriteColor = input()
+
+print(fullName + "Your are " + yourAge + " and you favorite is " + favoriteColor)
